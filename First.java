518473fdbcddcc8fc114d178391d3ea55aa0d27f
Lifni1998 <lifni1998@gmail.com>
2025-03-07 11:57:41 +0530

    second

--- First.java
+++ First.java
@@ -3,5 +3,6 @@ package Welcome-git;
 public class First {
     public static void main(String args[]){
         System.out.println("WELCOME");
+        System.out.println("HELLO WORLD!!!");
     }
 }
