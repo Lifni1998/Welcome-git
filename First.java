90350b08edc47dd344c9b60de8a668022f873189
Lifni1998 <lifni1998@gmail.com>
2025-03-07 14:28:53 +0530

    Update First.java

--- First.java
+++ First.java
@@ -2,7 +2,7 @@ package Welcome-git;
 
 public class First {
     public static void main(String args[]){
-        System.out.println("WELCOME");
+        System.out.print("WELCOME");
         System.out.println("HELLO WORLD!!!");
         System.out.println("aaahhhh!!!");
         System.out.println("hi..hlo...");
