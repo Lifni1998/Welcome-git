5f8b5d193c10c4bd817da2a03136fc0c295017af
Lifni1998 <lifni1998@gmail.com>
2025-03-07 11:45:21 +0530

    first commit

---
+++ addition.java
@@ -0,0 +1,11 @@
+package Welcome-git;
+
+public class addition {
+    public static void main(String args[]){
+        int a=10;
+        int b=12;
+        int c;
+        c=a+b;
+        System.out.println("Result is:"+c);
+}
+}
