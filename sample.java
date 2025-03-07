affb521470a0ff8cb4221bafc03a112807a41f25
Lifni1998 <lifni1998@gmail.com>
2025-03-07 11:41:10 +0530

    second commit

--- sample.java
+++ sample.java
@@ -3,5 +3,10 @@ package Welcome-git;
 public class sample {
     public static void main(String args[]){
         System.out.println("WELCOME");
+        int a=10;
+        int b=7;
+        int c;
+        c=a+b;
+        System.out.println("Result is:"+c);
     }
 }
