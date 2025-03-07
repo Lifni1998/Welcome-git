120052333e117ba3156728479677ad12f49c6794
Lifni1998 <lifni1998@gmail.com>
2025-03-07 13:27:42 +0530

    hi

---
+++ a.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class a {
+    public static void main(String args[]){
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int b=obj.nextInt();
+        System.out.println("Enter the second number:");
+        int c=obj.nextInt();
+        int d;
+        d=b+c;
+        System.out.println("Result is:"+d);
+}
+}
